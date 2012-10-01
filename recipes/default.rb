cf00d4b3de49608a388efc4699493cbd8b895590
Patrick Debois <Patrick.Debois@jedi.be>
2012-10-01 13:42:49 +0200

    updated metadata copyright

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,22 +20,18 @@
 include_recipe "git"
 include_recipe "collectd::server"
 
-git "/opt/collectd-graphite-writer" do
-  repository "git://github.com/indygreg/collectd-carbon.git"
-  reference "939e9f8eb6d782a1cecf76d9b1b2a8b2b2632ba2"
+git "/opt/collectd-elasticsearch" do
+  repository "git://github.com/phobos182/collectd-elasticsearch.git"
+  reference "2da36b96d4a8ac5581ec95f3bd8e8da360c726e2"
   action :sync
 #  notifies :run, "execute[build debian package]"
 end
 
-collectd_python_plugin "carbon_writer" do
-    options  :line_receiver_host      => "127.0.0.1",
-        :line_receiver_port               => 2003,
-        :differentiate_counters_over_time => true,
-        :lowercase_metric_names           => true,
-        :types_d_b                        => "/usr/share/collectd/types.db",
-        :metric_prefix                    => "collectd"
+collectd_python_plugin "elasticsearch" do
+    options  'Verbose' => false,
+        :cluster => "elasticsearch"
 end
 
-link "#{node[:collectd][:plugin_dir]}/carbon_writer.py" do
-    to "/opt/collectd-graphite-writer/carbon_writer.py"
+link "#{node[:collectd][:plugin_dir]}/elasticsearch.py" do
+    to "/opt/collectd-elasticsearch/elasticsearch.py"
 end
